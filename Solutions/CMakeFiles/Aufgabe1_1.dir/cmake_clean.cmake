55845f8e8c0850269ab4012f04fab67faaf4c90a
jannispriesnitz <jannis-priesnitz@freenet.de>
2016-11-30 23:48:32 +0100

    doku 2

---
+++ Solutions/CMakeFiles/Aufgabe1_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Aufgabe1_1.dir/Aufgabe1_1.cpp.o"
+  "../bin/Debug/Aufgabe1_1.pdb"
+  "../bin/Debug/Aufgabe1_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aufgabe1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Solutions/CMakeFiles/Aufgabe2_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Aufgabe2_1.dir/Aufgabe2_1.cpp.o"
+  "../bin/Debug/Aufgabe2_1.pdb"
+  "../bin/Debug/Aufgabe2_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aufgabe2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
